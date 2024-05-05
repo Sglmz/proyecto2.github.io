49ce3c02a40aba4b652e81c19dc5d04610f41c3d
Sglmzd <126720803+Sglmzd@users.noreply.github.com>
2024-05-04 21:03:05 -0600

    Proyecto
    asdadsa

---
+++ Proyecto2/login/login.js
@@ -0,0 +1,16 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    
+    var storedUsername = localStorage.getItem('username');
+    var storedPassword = localStorage.getItem('password');
+    
+    if (username === storedUsername && password === storedPassword) {
+      alert('Bienvenido!');
+      window.location.href = '../index.html';
+    } else {
+      alert('Nombre de usuario o contraseña incorrectos.');
+    }
+  });
---
+++ Proyecto2/login/register.js
@@ -0,0 +1,12 @@
+document.getElementById('registerForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    localStorage.setItem('username', username);
+    localStorage.setItem('password', password);
+  
+    alert('Usuario creado con éxito');
+    window.location.href = 'login.html';
+  });
+  
